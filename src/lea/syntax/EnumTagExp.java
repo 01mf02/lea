f47dbd11b8eac83c7398a431638bc7213c597d08
Michaël Hébert-Bédard <mic_hb@hotmail.com>
2012-12-04 14:20:14 +0100

    Expressions corrigées pour cup (Bool and Number)

---
+++ src/lea/syntax/EnumTagExp.java
@@ -0,0 +1,31 @@
+package lea.syntax;
+
+public enum EnumTagExp 
+{
+	AND ("&&"), 
+	OR ("||"), 
+	LT ("<"), 
+	GT (">"), 
+	LE ("<="), 
+	GE (">="), 
+	EQ ("=="), 
+	DIFF ("!=") , 
+	PLUS ("+"), 
+	MINUS ("-"), 
+	MINUS_U ("-"), 
+	MOD ("%"), 
+	MULT ("*"), 
+	DIV ("/");
+	
+	
+    private final String tag;
+
+    EnumTagExp(String s){
+            tag = s;
+    }
+
+    public String toString(){
+            return tag;
+    }
+}
+
