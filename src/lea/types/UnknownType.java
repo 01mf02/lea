44c7aa1e510a300cf7e0c357e42695dc534fc290
Michaël Hébert-Bédard <mic_hb@hotmail.com>
2012-12-09 19:36:28 +0100

    Un fichier en oubli (oups)

---
+++ src/lea/types/UnknownType.java
@@ -0,0 +1,22 @@
+package lea.types;
+
+import lea.constants.*;
+
+public class UnknownType extends Type 
+{
+	public UnknownType()
+	{
+	}
+
+	@Override
+	public boolean equals(Type t1) 
+	{
+		return false;
+	}
+
+	@Override
+	public boolean equals(Constant c1) 
+	{
+		return false;
+	}
+}
