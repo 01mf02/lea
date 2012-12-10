a8c94d0f6bbff0713a7586e1cf84df51a97b7505
Michael Färber <01mf02@gmail.com>
2012-12-10 23:58:18 +0100

    Refactor CharConstant, extend TODO.

--- src/lea/constants/CharConstant.java
+++ src/lea/constants/CharConstant.java
@@ -1,30 +1,26 @@
 package lea.constants;
 
-import lea.types.*;
+import lea.types.CharType;
+import lea.types.Type;
 
-public class CharConstant implements Constant 
-{
-	char _value = ' ';
+public class CharConstant implements Constant {
+	char value = ' ';
 	Type t;
-	
-	public CharConstant(String value)
-	{
-		_value = value.charAt(0);
+
+	public CharConstant(String v) {
+		value = v.charAt(0);
 		t = new CharType();
 	}
-    
-    public Type getType()
-    {
-    	return t;
-    }
-    
-    public char getValue()
-    {
-    	return _value;
-    }
-    
-    public String toString()
-    {
-    	return Character.toString(_value);
-    }
+
+	public Type getType() {
+		return t;
+	}
+
+	public char getValue() {
+		return value;
+	}
+
+	public String toString() {
+		return Character.toString(value);
+	}
 }
