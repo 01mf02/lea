65cceb4e2e366869a15cad49d16a826b3e90b255
Michael Färber <01mf02@gmail.com>
2012-12-10 23:30:45 +0100

    Remove unneeded comments

--- src/lea/syntax/ListNode.java
+++ src/lea/syntax/ListNode.java
@@ -1,45 +1,23 @@
 package lea.syntax;
-import lea.types.*;
 
-public class ListNode extends Expression 
-{
-	public ListNode(Expression a1, Expression a2)
-	{
+import lea.types.TupleType;
+import lea.types.Type;
+
+public class ListNode extends Expression {
+	public ListNode(Expression a1, Expression a2) {
 		super(a1, a2);
 	}
-	
-	public String toString()
-	{
-		return "ListNode"+super.toString();
+
+	public String toString() {
+		return "ListNode" + super.toString();
 	}
-	
-	public Type getType()
-	{		
-		return new TupleType(this.getLeft().getType(), this.getRight().getType());
+
+	public Type getType() {
+		return new TupleType(this.getLeft().getType(), this.getRight()
+				.getType());
 	}
-	
-	public String toDotString()
-	{
+
+	public String toDotString() {
 		return "ListNode";
 	}
 }
-
-
-/*
-
-
-
-parser code  {: 
- public void report_fatal_error(String message, Object info) 
-  {
-  	java_cup.runtime.Symbol symb = (java_cup.runtime.Symbol)info;
-  	Main.printError("Ligne : " + symb.left + " Colonne : " + symb.right + "   : " + message, 2);
-  }
-  
-  public void report_error(String message, Object info) 
-  {
-  	java_cup.runtime.Symbol symb = (java_cup.runtime.Symbol)info;
-  	Main.printError("Ligne : " + symb.left + " Colonne : " + symb.right + "   : " + message, 1);
-  }
-:};
-*/
