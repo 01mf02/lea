4007c86fd117b09f57aff0fa16c43b0ca6ceeca0
Michael Farber <mfarber@emi.u-bordeaux1.fr>
2012-12-18 15:23:10 +0100

    Simplify simple.lea, implement return node.

--- src/lea/syntax/ReturnNode.java
+++ src/lea/syntax/ReturnNode.java
@@ -1,15 +1,25 @@
 package lea.syntax;
 
+import lea.generator.CodeWriter;
+
 public class ReturnNode extends Instruction 
 {
+	Expression return_expr;
+	
 	public ReturnNode(Expression a1)
 	{
 		super(a1, null);
+		return_expr = a1;
 	}
 	
 	public String toString()
 	{
 		return "Return"+super.toString();
+	}
+	
+	public void toJava(CodeWriter cw)
+	{
+		cw.writeLine("return " + return_expr.toJava() + ";");
 	}
 	
 	public String toDotString()
