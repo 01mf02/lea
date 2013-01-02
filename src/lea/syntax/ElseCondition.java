24aab28230eeaef73db7284a25d53f77d9dfb90d
Michael Färber <01mf02@gmail.com>
2013-01-02 10:56:05 +0100

    Implement toJava() of else condition.

--- src/lea/syntax/ElseCondition.java
+++ src/lea/syntax/ElseCondition.java
@@ -1,8 +1,15 @@
 package lea.syntax;
 
+import lea.generator.CodeWriter;
+
 public class ElseCondition extends Instruction {
-	public ElseCondition(SyntaxTree a1, SyntaxTree a2) {
+	private Instruction if_instruction = null;
+	private Instruction else_instruction = null;
+
+	public ElseCondition(Instruction a1, Instruction a2) {
 		super(a1, a2);
+		if_instruction = a1;
+		else_instruction = a2;
 	}
 
 	public String toString() {
@@ -13,7 +20,14 @@ public class ElseCondition extends Instruction {
 		return "Else";
 	}
 
-	public String toJava() {
-		return "else";
+	public void toJava(CodeWriter w) {
+		if_instruction.toJava(w);
+		// block was opened before by "if"
+		w.closeBlock();
+
+		w.writeLine("else");
+		w.openBlock();
+		else_instruction.toJava(w);
+		// block doesn't need to be closed, "if" cares about that
 	}
 }
