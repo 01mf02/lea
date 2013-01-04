7dd756dc2b42e5affcbc34874cf37b8972212987
Michael Färber <01mf02@gmail.com>
2013-01-04 17:55:42 +0100

    Uncomment that crazy code.

--- src/lea/syntax/Assignment.java
+++ src/lea/syntax/Assignment.java
@@ -1,7 +1,6 @@
 package lea.syntax;
 
 import lea.generator.CodeWriter;
-import lea.types.StructType;
 
 public class Assignment extends Instruction {
 	Expression assignment_left, assignment_right;
@@ -17,27 +16,35 @@ public class Assignment extends Instruction {
 	}
 
 	public void toJava(CodeWriter w) {
-		if (assignment_right != null) {
-			String leftPart = assignment_left.toJava();
-			String rightPart = assignment_right.toJava();
+		if (assignment_right != null)
+			w.writeLine(assignment_left.getType().toJava() + " = "
+					+ assignment_right.toJava() + ";");
+		else
+			w.writeLine(assignment_left.getType().toJava() + " "
+					+ assignment_left.toJava() + ";");
 
-			if (assignment_right instanceof TupleNode)
-				rightPart = "new Object[] {"
-						+ rightPart.substring(1).substring(0,
-								rightPart.length() - 2) + "}";
+		// That code makes my eyes bleed.
 
-			w.writeLine(leftPart + " = " + rightPart + ";");
-		} else {
-			if (assignment_left.getType() instanceof StructType) {
-				w.writeLine(((StructType) assignment_left.getType()).toJava()
-						+ " " + assignment_left.toJava() + " = new "
-						+ ((StructType) assignment_left.getType()).toJava()
-						+ "();");
-			} else
-				w.writeLine(assignment_left.getType().toJava() + " "
-						+ assignment_left.toJava() + ";");
-		}
-
+		// if (assignment_right != null) {
+		// String leftPart = assignment_left.toJava();
+		// String rightPart = assignment_right.toJava();
+		//
+		// if (assignment_right instanceof TupleNode)
+		// rightPart = "new Object[] {"
+		// + rightPart.substring(1).substring(0,
+		// rightPart.length() - 2) + "}";
+		//
+		// w.writeLine(leftPart + " = " + rightPart + ";");
+		// } else {
+		// if (assignment_left.getType() instanceof StructType) {
+		// w.writeLine(((StructType) assignment_left.getType()).toJava()
+		// + " " + assignment_left.toJava() + " = new "
+		// + ((StructType) assignment_left.getType()).toJava()
+		// + "();");
+		// } else
+		// w.writeLine(assignment_left.getType().toJava() + " "
+		// + assignment_left.toJava() + ";");
+		// }
 	}
 
 	public String toDotString() {
