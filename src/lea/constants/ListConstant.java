574951ed7daeec8fbe466d43cbaf5a29608b9ff6
Michael Färber <01mf02@gmail.com>
2013-01-05 19:32:31 +0100

    Make list constants work again.

--- src/lea/constants/ListConstant.java
+++ src/lea/constants/ListConstant.java
@@ -1,62 +1,25 @@
 package lea.constants;
 
-import java.util.LinkedList;
-
-import lea.syntax.ConstantLeaf;
-import lea.syntax.Expression;
-import lea.syntax.ListNode;
-import lea.types.ListType;
+import lea.syntax.List;
 import lea.types.Type;
 
 public class ListConstant implements Constant {
-	LinkedList<Constant> value = null;
-	ListType listType;
 
-	public ListConstant(ListNode ln, ListType t) {
-		Expression tmp = (Expression) ln.getLeft();
-		LinkedList<Constant> nodesValues = new LinkedList<Constant>();
+	private List list;
 
-		// Obtient tous les noeuds
-		if (tmp.getLeft() != null || tmp.getRight() != null) {
-			while ((tmp.getLeft() != null) || (tmp.getRight() != null)) {
-				if (tmp.getRight() != null) {
-					if (tmp.getRight().getLeft() == null
-							&& tmp.getRight().getRight() == null) {
-						ConstantLeaf c = (ConstantLeaf) tmp.getRight();
-						nodesValues.add(0, c.getValue());
-					}
-				}
-				if (tmp.getLeft() != null) {
-					if (tmp.getLeft().getLeft() == null
-							&& tmp.getLeft().getRight() == null) {
-						ConstantLeaf c = (ConstantLeaf) tmp.getLeft();
-						nodesValues.add(0, c.getValue());
-					}
-				}
-				tmp = (Expression) tmp.getLeft();
-			}
-		}
-		value = nodesValues;
-		listType = t;
+	public ListConstant(List l) {
+		list = l;
 	}
 
 	public Type getType() {
-		return listType;
+		return list.getType();
 	}
 
 	public String toString() {
-		return value.toString();
+		return list.toString();
 	}
 
 	public String toJava() {
-		String l = "{";
-		for (int i = 0; i < value.size(); i++) {
-			l += value.get(i).toJava();
-			if (i < value.size() - 1)
-				l += ", ";
-		}
-		l += "}";
-
-		return l;
+		return list.toJava();
 	}
 }
